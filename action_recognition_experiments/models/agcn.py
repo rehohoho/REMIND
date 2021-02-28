278049b1f04da5f259da8d9fa0f2943a2fe12bf9
rehohoho <ernie.birdy.ho@gmail.com>
2021-02-28 14:34:20 +0800

    Split AGCN model.

--- action_recognition_experiments/models/agcn.py
+++ action_recognition_experiments/models/agcn.py
@@ -142,16 +142,18 @@ class Model(nn.Module):
         A = self.graph.A
         self.data_bn = nn.BatchNorm1d(num_person * in_channels * num_point)
 
-        self.l1 = TCN_GCN_unit(3, 64, A, residual=False)
-        self.l2 = TCN_GCN_unit(64, 64, A)
-        self.l3 = TCN_GCN_unit(64, 64, A)
-        self.l4 = TCN_GCN_unit(64, 64, A)
-        self.l5 = TCN_GCN_unit(64, 128, A, stride=2)
-        self.l6 = TCN_GCN_unit(128, 128, A)
-        self.l7 = TCN_GCN_unit(128, 128, A)
-        self.l8 = TCN_GCN_unit(128, 256, A, stride=2)
-        self.l9 = TCN_GCN_unit(256, 256, A)
-        self.l10 = TCN_GCN_unit(256, 256, A)
+        self.levels = nn.ModuleList([
+            TCN_GCN_unit(3, 64, A, residual=False),
+            TCN_GCN_unit(64, 64, A),
+            TCN_GCN_unit(64, 64, A),
+            TCN_GCN_unit(64, 64, A),
+            TCN_GCN_unit(64, 128, A, stride=2),
+            TCN_GCN_unit(128, 128, A),
+            TCN_GCN_unit(128, 128, A),
+            TCN_GCN_unit(128, 256, A, stride=2),
+            TCN_GCN_unit(256, 256, A),
+            TCN_GCN_unit(256, 256, A),
+        ])
 
         self.fc = nn.Linear(256, num_class)
         nn.init.normal_(self.fc.weight, 0, math.sqrt(2. / num_class))
@@ -164,20 +166,62 @@ class Model(nn.Module):
         x = self.data_bn(x)
         x = x.view(N, M, V, C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
 
-        x = self.l1(x)
-        x = self.l2(x)
-        x = self.l3(x)
-        x = self.l4(x)
-        x = self.l5(x)
-        x = self.l6(x)
-        x = self.l7(x)
-        x = self.l8(x)
-        x = self.l9(x)
-        x = self.l10(x)
-
+        for level in self.levels:
+            x = level(x)
+        
         # N*M,C,T,V
         c_new = x.size(1)
         x = x.view(N, M, c_new, -1)
         x = x.mean(3).mean(1)
 
         return self.fc(x)
+
+
+class AGCN_ClassifyAfterLevel(Model):
+    
+    def __init__(self, level, num_class=60, num_point=25, num_person=2, graph=None, graph_args=dict(), in_channels=3):
+        super(AGCN_ClassifyAfterLevel, self).__init__(num_class, num_point, num_person, graph, graph_args, in_channels)
+
+        for i in range(10-level):
+            del self.levels[-1]
+
+
+class AGCN_StartAtLevel(Model):
+
+    def __init__(self, level, num_class=60, num_point=25, num_person=2, graph=None, graph_args=dict(), in_channels=3):
+        super(AGCN_StartAtLevel, self).__init__(num_class, num_point, num_person, graph, graph_args, in_channels)
+        
+        del self.data_bn
+        self.levels = self.levels[-(10-level+1):]
+
+    def forward(self, x):
+        for level in self.levels:
+            x = level(x)
+        
+        # N*M,C,T,V
+        c_new = x.size(1)
+        x = x.view(N, M, c_new, -1)
+        x = x.mean(3).mean(1)
+
+        return self.fc(x)
+
+
+if __name__ == '__main__':
+
+    model = Model(graph='graph.ntu_rgb_d.Graph', graph_args={'labeling_mode': 'spatial'})
+    g = AGCN_ClassifyAfterLevel(9, graph='graph.ntu_rgb_d.Graph', graph_args={'labeling_mode': 'spatial'})
+    f = AGCN_StartAtLevel(10, graph='graph.ntu_rgb_d.Graph', graph_args={'labeling_mode': 'spatial'})
+    
+    def get_name_to_module(model):
+        name_to_module = {}
+        for m in model.named_modules():
+            name_to_module[m[0]] = m[1]
+        return name_to_module
+
+    print('\n\norig')
+    print(list(get_name_to_module(model).keys()))
+    print('\n\ng')
+    print(list(get_name_to_module(g).keys())) # extract features from levels.8 or levels.8.relu
+    print('\n\nf')
+    print(list(get_name_to_module(f).keys()))
+    
