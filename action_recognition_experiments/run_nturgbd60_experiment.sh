3026b97b4f108026122fe3d387c6981b16b1e85b
rehohoho <ernie.birdy.ho@gmail.com>
2021-03-07 12:49:16 +0800

    Add indices for 50-10 split. Modify run script.

--- action_recognition_experiments/run_nturgbd60_experiment.sh
+++ action_recognition_experiments/run_nturgbd60_experiment.sh
@@ -6,44 +6,45 @@ export PYTHONPATH=${PROJ_ROOT}
 export KMP_DUPLICATE_LIB_OK=TRUE
 cd ${PROJ_ROOT}/action_recognition_experiments
 
-NTURGBD60_ROOT=/home/ruien/GradientEpisodicMemory/data/raw ##
-EXPT_NAME=remind_nturgbd60
+DATA_TYPE=joint
+NTURGBD60_ROOT=/home/ruien/2s-AGCN/data/ntu/xsub ##
+EXPT_NAME=ntu60xsub${DATA_TYPE}
 GPU=0 ##
 
 
 CODEBOOK_SIZE=256
 NUM_CODEBOOKS=32
-REPLAY_SAMPLES=23 # batch number -1
+REPLAY_SAMPLES=15 # batch number -1
 MAX_BUFFER_SIZE=959665
 
-BASE_INIT_CLASSES=6
-CLASS_INCREMENT=6
+BASE_INIT_CLASSES=50 # 6
+CLASS_INCREMENT=1 # 6
 NUM_CLASSES=60
-BASE_INIT_CKPT=./nturgbd60_2sagcn_ckpts/best_AGCN_ClassifyAfterLevel_6.pth # base init ckpt file
+BASE_INIT_CKPT=./files/50c210c_pretrained_without_blocks.pt # base init ckpt file
 LABEL_ORDER_DIR=./files/indices # location of numpy label files
 
 
 #set +o posix ##
 #exec > >(tee run_nturgbd60_experiment.log) 2>&1 ##
-export CUDA_VISIBLE_DEVICES=${GPU} 
+export CUDA_VISIBLE_DEVICES=${GPU}
 python -u nturgbd60_experiment.py \
 --expt_name ${EXPT_NAME} \
 --label_dir ${LABEL_ORDER_DIR} \
---train_data_path=${NTURGBD60_ROOT}/train_data_joint.npy \
+--train_data_path=${NTURGBD60_ROOT}/train_data_${DATA_TYPE}.npy \
 --train_label_path=${NTURGBD60_ROOT}/train_label.pkl \
---val_data_path=${NTURGBD60_ROOT}/val_data_joint.npy \
+--val_data_path=${NTURGBD60_ROOT}/val_data_${DATA_TYPE}.npy \
 --val_label_path=${NTURGBD60_ROOT}/val_label.pkl \
---base_arch "AGCN_ClassifyAfterLevel" \
---base_model_args "{level: 9, num_class: 60, num_point: 25, num_person: 2, graph: graph.ntu_rgb_d.Graph, graph_args: {labeling_mode: 'spatial'}}" \
---classifier "AGCN_StartAtLevel" \
---classifier_model_args "{level: 10, num_class: 60, num_point: 25, num_person: 2, graph: graph.ntu_rgb_d.Graph, graph_args: {labeling_mode: 'spatial'}}" \
+--base_arch "MSG3D_EndAtSCGN3" \
+--base_model_args "{num_class: 60, num_point: 25, num_person: 2, num_gcn_scales: 13, num_g3d_scales: 6, graph: graph.ntu_rgb_d.AdjMatrixGraph}" \
+--classifier "MSG3D_StartAfterSCGN3" \
+--classifier_model_args "{num_class: 60, num_point: 25, num_person: 2, num_gcn_scales: 13, num_g3d_scales: 6, graph: graph.ntu_rgb_d.AdjMatrixGraph}" \
 --classifier_ckpt ${BASE_INIT_CKPT} \
---extract_features_from levels.8 \
---num_channels 256 \
+--extract_features_from sgcn3.2 \
+--num_channels 384 \
 --num_instances 2 \
 --spatial_feat_dim 75,25 \
 --weight_decay 1e-5 \
---batch_size 24 \
+--batch_size 16 \
 --num_codebooks ${NUM_CODEBOOKS} \
 --codebook_size ${CODEBOOK_SIZE} \
 --rehearsal_samples ${REPLAY_SAMPLES} \
@@ -59,3 +60,10 @@ python -u nturgbd60_experiment.py \
 --class_increment ${CLASS_INCREMENT} \
 --streaming_min_class ${BASE_INIT_CLASSES} \
 --streaming_max_class ${NUM_CLASSES}
+
+# --base_arch "AGCN_ClassifyAfterLevel" \
+# --base_model_args "{level: 9, num_class: 60, num_point: 25, num_person: 2, graph: graph.ntu_rgb_d.Graph, graph_args: {labeling_mode: 'spatial'}}" \
+# --classifier "AGCN_StartAtLevel" \
+# --classifier_model_args "{level: 10, num_class: 60, num_point: 25, num_person: 2, graph: graph.ntu_rgb_d.Graph, graph_args: {labeling_mode: 'spatial'}}" \ 
+# --extract_features_from levels.8 \
+# --num_channels 256 \
